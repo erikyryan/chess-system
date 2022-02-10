2f71f29e6b7aee20d81e3b9f5f97bdff12cb916f
Eriky Ryan <erikyryang@gmail.com>
2022-02-10 10:01:29 -0300

    ChessException and ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,21 @@ package application;
 
 import chess.ChessMatch;
 
+
 public class Program {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
     }
 }
+
+/*
+
+matrix_row = 8 - chess_row
+
+matrix_column = chess_column - 'a'
+
+'a' - 'a' = 0
+'b' - 'a' = 1
+
+*/
